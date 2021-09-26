4bf8246939e75c58a514729c132abc0829635db6
tgrosson <54158956+tgrosson@users.noreply.github.com>
2021-09-26 14:04:28 -0500

    Setup

---
+++ README.md
@@ -0,0 +1,57 @@
+
+# MusicBot
+Custom music player Discord bot based on jagrosh's [JMusicBot](https://jmusicbot.com) ([github](https://github.com/jagrosh/MusicBot)). Most of the content on this repo was written by jagrosh, and edits have been made in accordance with the [Apache 2.0](https://github.com/tgrosson/MusicBot/blob/master/LICENSE) license.
+
+If you like this bot, be sure to add a star to the orginal bot, [**JMusicBot**](https://github.com/jagrosh/MusicBot), as well as the libraries that make this possible: [**JDA**](https://github.com/DV8FromTheWorld/JDA) and [**lavaplayer**](https://github.com/sedmelluq/lavaplayer)!
+
+[![Release](https://img.shields.io/github/v/release/tgrosson/MusicBot)](https://github.com/tgrosson/MusicBot/releases/latest)
+[![License](https://img.shields.io/github/license/tgrosson/MusicBot)](https://github.com/tgrosson/MusicBot/blob/master/LICENSE)
+
+
+A cross-platform Discord music bot with a clean interface, and that is easy to set up and run yourself!
+
+[![Setup](http://i.imgur.com/VvXYp5j.png)](https://jmusicbot.com/setup)
+
+## Features
+  * Easy to run (just make sure Java is installed, and run!)
+  * Fast loading of songs
+  * No external keys needed (besides a Discord Bot token)
+  * Smooth playback
+  * Server-specific setup for the "DJ" role that can moderate the music
+  * Clean and beautiful menus
+  * Supports many sites, including Youtube, Soundcloud, and more
+  * Supports many online radio/streams
+  * Supports local files
+  * Playlist support (both web/youtube, and local)
+
+## Supported sources and formats
+MusicBot/JMusicBot supports all sources and formats supported by [lavaplayer](https://github.com/sedmelluq/lavaplayer#supported-formats):
+### Sources
+  * YouTube
+  * SoundCloud
+  * Bandcamp
+  * Vimeo
+  * Twitch streams
+  * Local files
+  * HTTP URLs
+### Formats
+  * MP3
+  * FLAC
+  * WAV
+  * Matroska/WebM (AAC, Opus or Vorbis codecs)
+  * MP4/M4A (AAC codec)
+  * OGG streams (Opus, Vorbis and FLAC codecs)
+  * AAC streams
+  * Stream playlists (M3U and PLS)
+
+## Example
+![Loading Example...](https://i.imgur.com/kVtTKvS.gif)
+
+## Setup
+Please see JMusicBot's [Setup Page](https://jmusicbot.com/setup) to run this bot yourself! The setup for this bot is the same as for JMusicBot.
+
+## Questions/Suggestions/Bug Reports
+If you found this repository online and have a suggestion/bug report, I highly recommend you use [JMusicBot](https://jmusicbot.com) instead of this one. This bot was intended for private use and is not regularly monitored for issues. If you have a suggestion for this bot, it's best to contact tgrosson directly.
+
+## Editing
+jagrosh says: "[JMusicBot] (and the source code) might not be easy to edit for inexperienced programmers. The main purpose of having the source public is to show the capabilities of the libraries, to allow others to understand how the bot works, and to allow those knowledgeable about java, JDA, and Discord bot development to contribute." That being said, I'm not a Java developer, and I was eventually able to figure it out, so feel free to play around with the code if you wish. If you choose to make edits, please do so in accordance with the Apache 2.0 License, as was done for this bot.
---
+++ src/main/java/com/tgrosson/bkmusicbot/Listener.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2016 John Grosh (jagrosh).
+ * Modified 2021 Ted Grosson (tgrosson).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.tgrosson.bkmusicbot;
+
+import com.tgrosson.bkmusicbot.utils.OtherUtil;
+import java.util.concurrent.TimeUnit;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.VoiceChannel;
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.ShutdownEvent;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageDeleteEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author John Grosh (jagrosh)
+ */
+public class Listener extends ListenerAdapter
+{
+    private final Bot bot;
+    
+    public Listener(Bot bot)
+    {
+        this.bot = bot;
+    }
+    
+    @Override
+    public void onReady(ReadyEvent event) 
+    {
+        if(event.getJDA().getGuildCache().isEmpty())
+        {
+            Logger log = LoggerFactory.getLogger("MusicBot");
+            log.warn("This bot is not on any guilds! Use the following link to add the bot to your guilds!");
+            log.warn(event.getJDA().getInviteUrl(BKMusicBot.RECOMMENDED_PERMS));
+        }
+        event.getJDA().getGuilds().forEach((guild) -> 
+        {
+            try
+            {
+                String defpl = bot.getSettingsManager().getSettings(guild).getDefaultPlaylist();
+                VoiceChannel vc = bot.getSettingsManager().getSettings(guild).getVoiceChannel(guild);
+                if(defpl!=null && vc!=null && bot.getPlayerManager().setUpHandler(guild).playFromDefault())
+                {
+                    guild.getAudioManager().openAudioConnection(vc);
+                }
+            }
+            catch(Exception ignore) {}
+        });
+        if(bot.getConfig().useUpdateAlerts())
+        {
+            bot.getThreadpool().scheduleWithFixedDelay(() -> 
+            {
+                User owner = bot.getJDA().getUserById(bot.getConfig().getOwnerId());
+                if(owner!=null)
+                {
+                    String currentVersion = OtherUtil.getCurrentVersion();
+                    String latestVersion = OtherUtil.getLatestVersion();
+                    if(latestVersion!=null && !currentVersion.equalsIgnoreCase(latestVersion))
+                    {
+                        String msg = String.format(OtherUtil.NEW_VERSION_AVAILABLE, currentVersion, latestVersion);
+                        owner.openPrivateChannel().queue(pc -> pc.sendMessage(msg).queue());
+                    }
+                }
+            }, 0, 24, TimeUnit.HOURS);
+        }
+    }
+    
+    @Override
+    public void onGuildMessageDelete(GuildMessageDeleteEvent event) 
+    {
+        bot.getNowplayingHandler().onMessageDelete(event.getGuild(), event.getMessageIdLong());
+    }
+
+    @Override
+    public void onGuildVoiceUpdate(@NotNull GuildVoiceUpdateEvent event)
+    {
+        bot.getAloneInVoiceHandler().onVoiceUpdate(event);
+    }
+
+    @Override
+    public void onShutdown(ShutdownEvent event) 
+    {
+        bot.shutdown();
+    }
+}
